762436d32147e83926506633b8e64df9e230aa40
ryanbs183 <ryan.sullivan.183@gmail.com>
2021-11-04 17:00:01 -0400

    fix target configuration

---
+++ src/VETCON_boot/Debug/main.d
@@ -0,0 +1,21 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: /Applications/ti/ccs1100/ccs/ccs_base/msp430/include/msp430.h
+main.obj: /Applications/ti/ccs1100/ccs/ccs_base/msp430/include/msp430fr2433.h
+main.obj: /Applications/ti/ccs1100/ccs/ccs_base/msp430/include/in430.h
+main.obj: /Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics.h
+main.obj: /Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics_legacy_undefs.h
+
+../main.c:
+
+/Applications/ti/ccs1100/ccs/ccs_base/msp430/include/msp430.h:
+
+/Applications/ti/ccs1100/ccs/ccs_base/msp430/include/msp430fr2433.h:
+
+/Applications/ti/ccs1100/ccs/ccs_base/msp430/include/in430.h:
+
+/Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics.h:
+
+/Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics_legacy_undefs.h:
+
