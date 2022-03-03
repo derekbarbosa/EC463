edc9f096ed86ea16dc72691de121333304234988
Derek Barbosa <dbarbosa@bu.edu>
2022-03-03 16:00:36 -0500

    removing CCStudio SRC library

--- src/OutOfBox_MSP430FR2433/Debug/iqmathlib/libraries/CCS/MPY32/5xx_6xx/subdir_vars.mk
+++
@@ -1,9 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-# Add inputs and outputs from these tool invocations to the build variables 
-LIB_SRCS += \
-../iqmathlib/libraries/CCS/MPY32/5xx_6xx/QmathLib_CCS_MPY32_5xx_6xx_CPUX_small_code_small_data.lib 
-
-
